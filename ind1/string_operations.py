f458e425db24f74aa6ef68f51cb121d0d8294f9a
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-14 17:03:02 +0300

    feat: Implement analyze_string function

--- ind1/string_operations.py
+++ ind1/string_operations.py
@@ -1,3 +1,10 @@
 def print_string(string):
     print(string)
 
+def analyze_string(string):
+    if string.isupper():
+        print(f"String {string} in uppercase")
+    elif string.islower():
+        print(f"String {string} in lowercase")
+    else:
+        print(f"String {string} is mixed")
