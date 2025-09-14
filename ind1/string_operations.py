505827e8257563f58011455913c15270d085af38
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-14 17:31:22 +0300

    feat: Implement type checking for all string_operations.py functions

--- ind1/string_operations.py
+++ ind1/string_operations.py
@@ -1,13 +1,23 @@
 def print_string(string):
-    print(string)
+    if type(string) is str:
+        print(string)
+    else:
+        raise TypeError("Data for print_string must be of type str")
 
 def analyze_string(string):
-    if string.isupper():
-        print(f"String {string} in uppercase")
-    elif string.islower():
-        print(f"String {string} in lowercase")
+    if type(string) is not str:
+        raise TypeError("Data for analyze_string must be of type str")
     else:
-        print(f"String {string} is mixed")
+        if string.isupper():
+            print(f"String {string} in uppercase")
+        elif string.islower():
+            print(f"String {string} in lowercase")
+        else:
+            print(f"String {string} is mixed")
+
 
 def to_uppercase(string):
-    return string.upper()
+    if type(string) is not str:
+        raise TypeError("Data for analyze_string must be of type str")
+    else:
+        return string.upper()
