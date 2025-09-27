7d7b5ad358ca6537ad0d6846bf9fb1542d611242
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-27 19:47:52 +0300

    feat: Implement JsonSaver class to save data to json format

---
+++ ind2/json_saver.py
@@ -0,0 +1,10 @@
+import json
+from typing import List
+
+from ind2.data_saver import DataSaver
+
+
+class JsonSaver(DataSaver):
+    def save(self, data_list: List[dict], filename: str):
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data_list, file, ensure_ascii=False, indent=4)
