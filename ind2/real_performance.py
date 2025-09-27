02a974420d6cff1bcf23c962fa682bfe50c569e9
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-27 18:16:30 +0300

    feat: Implement RealPerformance class with implementation of average_grade method

---
+++ ind2/real_performance.py
@@ -0,0 +1,12 @@
+from typing import List
+from ind2.performance import Performance
+
+
+class RealPerformance(Performance):
+    def __init__(self, subjects: List[str], real_grades: List[int]):
+        super().__init__(subjects, real_grades)
+
+    def average_grade(self) -> float:
+        if not self.__grades:
+            return 0
+        return sum(self.__grades) / len(self.__grades)
