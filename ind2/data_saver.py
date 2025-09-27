1ceedd0709e8b7538d157524e45573165ab5beec
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-27 19:11:34 +0300

    fix: Make save method use list of dictionaries instead of StudentData

--- ind2/data_saver.py
+++ ind2/data_saver.py
@@ -1,10 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from ind2.student_data import StudentData
-
 
 class DataSaver(ABC):
     @abstractmethod
-    def save(self, data_list: List[StudentData], filename: str):
+    def save(self, data_list: List[dict], filename: str):
         pass
