57a4cab4a3a82aeb66fcf28cf1ccf8d3105663dd
harpestes <dikaloandrij@gmail.com>
2025-11-09 16:41:11 +0200

    feat: Implement logging

--- ind5/app.py
+++ ind5/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import logging
 import time
 
 app = Flask(__name__)
@@ -6,26 +7,34 @@ app = Flask(__name__)
 start_time = time.time()
 request_count = 0
 
-
-@app.before_request
-def before_request():
-    global request_count
-    request_count += 1
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 
 @app.route('/')
 def home():
+    global request_count
+    request_count += 1
+    app.logger.info("Received request to '/' (Service work)")
     return "Сервіс працює", 200
 
 
 @app.route('/error')
 def error():
-    return 1 / 0
+    try:
+        1 / 0
+    except Exception as e:
+        app.logger.exception("Exception thrown on /error")
+    return "Виникла помилка (дивись app.log)", 500
 
 
 @app.route('/status')
 def status():
     uptime = time.time() - start_time
+    app.logger.info("Received request to /status")
     return jsonify({
         "uptime_seconds": round(uptime, 2),
         "total_requests": request_count
@@ -33,4 +42,4 @@ def status():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
