833e5bbdf527993247e06ebc1745c73db0d448a4
harpestes <dikaloandrij@gmail.com>
2025-11-09 17:29:11 +0200

    feat: Use UPD server in main server

--- ind5/app.py
+++ ind5/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import logging
 import time
+import socket
 
 app = Flask(__name__)
 
@@ -13,6 +14,18 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s"
 )
 
+UPD_SERVER_IP = "127.0.0.1"
+UPD_SERVER_PORT = 9999
+
+
+def send_to_upd_server(message: str):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(message.encode("utf-8"), (UPD_SERVER_IP, UPD_SERVER_PORT))
+        sock.close()
+    except Exception as e:
+        app.logger.warning(f"An error occurred during sending to UPD server: {e}")
+
 
 @app.route('/')
 def home():
@@ -24,15 +37,20 @@ def home():
 
 @app.route('/error')
 def error():
+    global request_count
+    request_count += 1
     try:
         1 / 0
     except Exception as e:
         app.logger.exception("Exception thrown on /error")
-    return "Виникла помилка (дивись app.log)", 500
+        send_to_upd_server(f"ERROR: {e}")
+        return "Виникла помилка (дивись app.log)", 500
 
 
 @app.route('/status')
 def status():
+    global request_count
+    request_count += 1
     uptime = time.time() - start_time
     app.logger.info("Received request to /status")
     return jsonify({
